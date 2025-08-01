437d2a4b20b61787e64422124447ceeb4affcb5a
dinhquoctoan <dinhquoctoan200103@gmail.com>
2025-08-01 20:18:27 +0700

    Them cac templates cho web app

--- scripts/test.py
+++ scripts/test.py
@@ -3,9 +3,9 @@ import pandas as pd
 from url_multi_labels_predictor import URL_PREDICTOR
 
 
-url = "https://www.allegrolokalnie.85xc.rest"
+url = "https://account.microsoft.com/account"
 
-predictor = URL_PREDICTOR(url)
-predictor.predict_with_RF(threshold=0.9, numerical=True)
+# predictor = URL_PREDICTOR(url)
+# predictor.predict_with_CNN(threshold=0.85, numerical=True)
 #predictor.df.to_csv("test.csv", index=False)
-predictor.print_result()
+# predictor.print_result()
